7ac1d78c6ef1c290b6f2f9b2fe26052225e5b119
khulanJargalsaikhan <onagraa@gmail.com>
2022-02-16 19:06:31 -0500

    canSum - Tabulation strategy

--- src/memorization_tabulation/CanSum.java
+++ src/memorization_tabulation/CanSum.java
@@ -1,34 +1,42 @@
 package memorization_tabulation;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CanSum {
 
 	public static void main(String[] args) {
+		System.out.println("-------- Memorization Strategy ---------");
 		System.out.println(canSum2(7, new int[] {5,3,4,7})); //true --> 3+4, 7 (2 possible way)
 		System.out.println(canSum2(7, new int[] {2,4})); //false 
 		System.out.println(canSum2(300, new int[] {7,14})); //false 
+		
+		System.out.println("-------- Tabulation Strategy ---------");
+		System.out.println(canSumTab(7, new int[] {5,3,4,7})); //true 
+		System.out.println(canSumTab(7, new int[] {2,4})); //false 
+		System.out.println(canSumTab(300, new int[] {7,14})); //false 
+		
 
 	}
-/* Easy-problem
+	/* Easy-problem
  Write a function canSum(targerSum, number) that takes in a targetSum and an array of numbers as arguments.
  The function should return a boolean indicating whether or not it is possible to generate the targetSum 
  using numbers from the array.
  You may use an element of the array as many times as needed.
  You may assume that all input numbers are nonnegative. 
- 
- */
+
+	 */
 	//recursive solution  
 	//m = targetSum
 	//n = nums.length
 	//time complexity O(n^m) which exponential
 	//space complexity O(m)
 	public static boolean canSum(int targetSum, int[] nums) {
-		
+
 		if (targetSum == 0) return true;
 		if (targetSum < 0) return false;
-		
+
 		for (int i=0; i<nums.length; i++) {
 			int remainder = targetSum - nums[i];
 			if (canSum(remainder, nums) == true) {
@@ -37,38 +45,38 @@ public class CanSum {
 		}
 		return false;
 	}
-	
-	
+
+
 	//memorized solution  
 	//m = targetSum
 	//n = nums.length
 	//time complexity O(n*m)
 	//space complexity O(m)
 	public static boolean canSum2(int targetSum, int[] nums) {
-		
+
 		Map<Integer, Boolean> memo = new HashMap<>();
-		
+
 		return canSumMemo(targetSum, nums, memo);
 	}
-	
+
 	private static boolean canSumMemo(int targetSum, int[] nums, Map<Integer, Boolean> memo) {
-		
+
 		//pulling the values from memo that already memorized 
 		if (memo.containsKey(targetSum)) {
 			return memo.get(targetSum);
 		}
-			
+
 		// if target == 0 then it mean targetSum is possible 
 		if (targetSum == 0) return true;
-		
+
 		// this means we gone too far, remainder can't be negative
 		if (targetSum < 0) return false;
-		
+
 		// iterate through array numbers
 		for (int i=0; i<nums.length; i++) {
 			//gives new targetSum
 			int remainder = targetSum - nums[i];
-			
+
 			// recursion call -> it returns true or false, if it's true that means there is a possibility
 			if (canSumMemo(remainder, nums, memo) == true) {
 				//caching the value
@@ -76,11 +84,31 @@ public class CanSum {
 				return true;
 			}
 		}
-		
+
 		// after attempted all possibilities, if none of them meets with true, then there is not possibility 
 		memo.put(targetSum, false);
 		return false;
 	}
-	
-	
-}
+
+
+	//Tabulation strategy  
+	//m = targetSum
+	//n = nums.length
+	//time complexity O(m*n)
+	//space complexity O(m)
+	public static boolean canSumTab(int targetSum, int[] nums) {
+		boolean[] table = new boolean[targetSum+1];
+		table[0] = true;
+		for(int i=0; i<=targetSum; i++) {
+			if(table[i] == true) {
+				for(int j=0; j<nums.length; j++) {
+					int k = i+nums[j];
+					if (k < table.length) table[k] = true;
+				}
+			}
+		}
+		return table[targetSum];
+		
+	}
+
+	}
