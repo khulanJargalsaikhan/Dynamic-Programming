c52c1364603f9715867ce7baf4adb9de5599d5db
khulanJargalsaikhan <onagraa@gmail.com>
2022-02-23 14:02:54 -0500

    canConstruct - Tabulation strategy

--- src/memorization_tabulation/CanConstruct.java
+++ src/memorization_tabulation/CanConstruct.java
@@ -10,11 +10,20 @@ public class CanConstruct {
 		System.out.println(canConstruct("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));  // false
 		System.out.println(canConstruct("", new String[] {"cat", "dog", "mouse"}));  // true (because it's base case)
 		//System.out.println(canConstruct1("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "ee", "eee", "eeee"}));  // false
-		
+
+		System.out.println("-------- Memorization Strategy ---------");
 		System.out.println(canConstructMemo("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd"}));   // true
 		System.out.println(canConstructMemo("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));  // false
 		System.out.println(canConstructMemo("", new String[] {"cat", "dog", "mouse"}));  // true (because it's base case)
 		System.out.println(canConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "ee", "eee", "eeee"}));  // false
+	
+		System.out.println("-------- Tabulation Strategy ---------");
+		System.out.println(canConstructMemo("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd"}));   // true
+		System.out.println(canConstructMemo("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));  // false
+		System.out.println(canConstructMemo("", new String[] {"cat", "dog", "mouse"}));  // true (because it's base case)
+		System.out.println(canConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "ee", "eee", "eeee"}));  // false
+	
+	
 	}
 	/*
 	 * Write a function 'canConstruct(target, wordBank)' that accepts a target string and an array of strings.
@@ -43,14 +52,14 @@ public class CanConstruct {
 		return false;
 	}
 
-
+	// Memorization strategy
 	// time: O(n*m^2)
 	// space: O(m^2)
 	public static boolean canConstructMemo(String target, String[] wordBank) {
 		Map<String, Boolean> memolist = new HashMap<>();
 		return canConstructMemo(target, wordBank, memolist);
 	}
-	
+
 	public static boolean canConstructMemo(String target, String[] wordBank, Map<String, Boolean> memolist) {
 		//adding base case for memorization
 		if(memolist.containsKey(target)) return memolist.get(target);
@@ -68,5 +77,29 @@ public class CanConstruct {
 		}
 		memolist.put(target, false);
 		return false;
+	}
+
+
+
+	//	Tabulation strategy  
+	//	m = targetSum
+	//	n = numbers.length 
+	//	Time: O(m^2 * n)  -  Space: O(m)
+	public static boolean canConstructTab(String target, String[] wordBank) {
+		//base case
+		boolean[] table = new boolean[target.length()+1];
+		table[0] = true;
+		
+		for (int i=0; i<=target.length(); i++) {
+			if(table[i] == true) {
+				for(String word : wordBank) {
+					// if the word matches the characters starting at position i
+					if(target.substring(i, i+word.length()).equals(word)) {
+						table[i+word.length()] = true;
+					}
+				}
+			}
+		}
+		return table[target.length()];
 	}
 }
