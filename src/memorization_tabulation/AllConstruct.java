af30de89cf3eb4c7e2b3bf631a38892e55d936e9
khulanJargalsaikhan <onagraa@gmail.com>
2022-02-27 19:27:56 -0500

    allConstruct - tabulation strategy (not fully working!)

--- src/memorization_tabulation/AllConstruct.java
+++ src/memorization_tabulation/AllConstruct.java
@@ -5,12 +5,25 @@ import java.util.ArrayList;
 public class AllConstruct {
 
 	public static void main(String[] args) {
-		System.out.println(allConstruct("purple", new String[] {"purp", "p", "ur", "le", "purpl"}));
+//		System.out.println("-------- Memorization Strategy ---------");
+//		System.out.println(allConstruct("purple", new String[] {"purp", "p", "ur", "le", "purpl"}));
+//		// 	[["purp", "le"], ["p", "ur", "p", "le"]]
+//		System.out.println(allConstruct("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
+//		// 	[["ab", "cd", "ef"], ["ab", "c", "def"], ["abc", "def"], ["abcd", "ef"]]
+//		System.out.println(allConstruct("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
+//		// 	[]
+		
+		
+		System.out.println("-------- Tabulation Strategy ---------");
+		//System.out.println(allConstructTab("purple", new String[] {"purp", "p", "ur", "le", "purpl"}));
 		// 	[["purp", "le"], ["p", "ur", "p", "le"]]
-		System.out.println(allConstruct("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
+		System.out.println(allConstructTab("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
 		// 	[["ab", "cd", "ef"], ["ab", "c", "def"], ["abc", "def"], ["abcd", "ef"]]
-		System.out.println(allConstruct("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
+		//System.out.println(allConstructTab("skateboard", new String[] {"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
 		// 	[]
+		
+		
+		
 	}
 	/*
 	 * Write a function 'allConstruct(target, wordBank)' that accepts a target string and an array of strings.
@@ -26,34 +39,82 @@ public class AllConstruct {
 	public static ArrayList<ArrayList<String>> allConstruct(String target, String[] wordBank){
 		//base case
 		if(target.equals("")) return new ArrayList<ArrayList<String>>();
-		
+
 		ArrayList<ArrayList<String>> result = new ArrayList<>();
 		ArrayList<String> inner = new ArrayList<>();
-		
+
 		for (int i=0; i<wordBank.length; i++) {
 			if(target.indexOf(wordBank[i]) == 0) {
 				String suffix = target.substring(wordBank[i].length());
 				ArrayList<ArrayList<String>> suffixWays = allConstruct(suffix, wordBank);
-				
-				
+
+
 				//this is not working!!!!
 				inner.add(0, wordBank[i]);
 				suffixWays.add(inner);
 				result.addAll(suffixWays);
-				
-				
+
+
 			}
-			
+
+		}
+
+		return result;
+	}
+
+
+		//	Tabulation strategy  
+		//	m = targetSum
+		//	n = numbers.length 
+		//	Time: O(n^m)  -  Space: O(n^m)   -> Exponential complexity
+
+	public static ArrayList<ArrayList<String>> allConstructTab(String target, String[] wordBank) {
+
+		@SuppressWarnings("unchecked")
+		ArrayList<ArrayList<String>>[] table = new ArrayList[target.length()+1];
+
+		//base case
+		table[0] = new ArrayList<ArrayList<String>>();
+
+
+		for (int i=0; i<=target.length(); i++) {
+			if (table[i] != null) {
+				for(String word : wordBank) {
+					// if the word matches the characters starting at position i
+					if(i+word.length()<table.length && target.substring(i, i+word.length()).equals(word)) {
+						
+						ArrayList<ArrayList<String>> temp = new ArrayList<>();
+						temp = table[i];
+
+						if (!temp.isEmpty()) {	
+							for (int k=0; k<temp.size(); k++) {
+								temp.get(k).add(word);
+							}
+						} else {
+							ArrayList<String> al = new ArrayList<>();
+							al.add(word);
+							temp.add(al);
+						}
+
+						if (table[i+word.length()] == null) {
+							table[i+word.length()] = new ArrayList<ArrayList<String>>();
+						}
+						 
+						table[i+word.length()].addAll(temp);
+						
+						temp.clear();
+
+					}
+					
+				}
+			}
 		}
 		
-		return result;
-		
-
-		
-		
-		
-		
-		
+		return table[target.length()];
 	}
+
+
+
+
 
 }
